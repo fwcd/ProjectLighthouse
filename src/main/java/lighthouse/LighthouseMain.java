d9c3667a0b6b3757ec205f421c2ea1e68d882d43
fwcd <fwcdmail@gmail.com>
2019-02-08 20:18:29 +0100

    Added more documentation

--- src/main/java/lighthouse/LighthouseMain.java
+++ src/main/java/lighthouse/LighthouseMain.java
@@ -1,15 +1,26 @@
 package lighthouse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import lighthouse.model.AppModel;
 import lighthouse.ui.AppFrame;
 
 /**
- * The application's entry point.
+ * The application's entry point. Instantiates
+ * fundamental parts of the program, such as the
+ * model or the window (called "frame").
  */
 public class LighthouseMain {
+	private static final Logger LOG = LoggerFactory.getLogger(LighthouseMain.class);
+	
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		
 		AppModel model = new AppModel();
 		AppFrame frame = new AppFrame(model);
 		frame.show();
+		
+		LOG.info("Launched application in {} ms", System.currentTimeMillis() - startTime);
 	}
 }
--- src/main/java/lighthouse/model/AppModel.java
+++ src/main/java/lighthouse/model/AppModel.java
@@ -4,7 +4,7 @@ package lighthouse.model;
  * The application's model.
  */
 public class AppModel {
-	/** The actual lighthouse grid dimensions. */
+	/** Use the lighthouse's grid dimensions. */
 	private final Grid grid = new Grid(28, 14);
 	
 	public Grid getGrid() {
