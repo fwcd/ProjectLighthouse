a79f047e984e9b61104b7cc44cbe472e4043e7f5
fwcd <fwcdmail@gmail.com>
2019-02-25 13:15:36 +0100

    WIP: Use lighthouse constants in AppModel

--- src/main/java/lighthouse/model/AppModel.java
+++ src/main/java/lighthouse/model/AppModel.java
@@ -1,11 +1,13 @@
 package lighthouse.model;
 
+import lighthouse.util.LighthouseConstants;
+
 /**
  * The application's model.
  */
 public class AppModel {
 	/** Use the lighthouse's grid dimensions. */
-	private final Grid grid = new Grid(28, 14);
+	private final Grid grid = new Grid(LighthouseConstants.LIGHTHOUSE_WIDTH, LighthouseConstants.LIGHTHOUSE_HEIGHT);
 	
 	public Grid getGrid() {
 		return grid;
