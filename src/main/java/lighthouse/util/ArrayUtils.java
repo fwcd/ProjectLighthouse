744cb1b95b7bc8e661421abcf14a069c470fdaa8
fwcd <fwcdmail@gmail.com>
2019-02-26 17:43:52 +0100

    Moved default parameter to the beginning in ArrayUtils to enhance readability

--- src/main/java/lighthouse/util/ArrayUtils.java
+++ src/main/java/lighthouse/util/ArrayUtils.java
@@ -8,56 +8,56 @@ public class ArrayUtils {
 	private ArrayUtils() {}
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static boolean getOr(boolean[] arr, int i, boolean defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static boolean getOr(boolean defaultValue, boolean[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static int getOr(int[] arr, int i, int defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static int getOr(int defaultValue, int[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static char getOr(char[] arr, int i, char defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static char getOr(char defaultValue, char[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static long getOr(long[] arr, int i, long defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static long getOr(long defaultValue, long[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static double getOr(double[] arr, int i, double defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static double getOr(double defaultValue, double[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static float getOr(float[] arr, int i, float defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static float getOr(float defaultValue, float[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static byte getOr(byte[] arr, int i, byte defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static byte getOr(byte defaultValue, byte[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static short getOr(short[] arr, int i, short defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static short getOr(short defaultValue, short[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value if the index is out of bounds */
-	public static <T> T getOr(T[] arr, int i, T defaultValue) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
+	public static <T> T getOr(T defaultValue, T[] arr, int i) { return (i >= 0 && i < arr.length) ? arr[i] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static boolean getOr(boolean[][] arr, int y, int x, boolean defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static boolean getOr(boolean defaultValue, boolean[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static int getOr(int[][] arr, int y, int x, int defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static int getOr(int defaultValue, int[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static char getOr(char[][] arr, int y, int x, char defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static char getOr(char defaultValue, char[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static long getOr(long[][] arr, int y, int x, long defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static long getOr(long defaultValue, long[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static double getOr(double[][] arr, int y, int x, double defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static double getOr(double defaultValue, double[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static float getOr(float[][] arr, int y, int x, float defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static float getOr(float defaultValue, float[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static byte getOr(byte[][] arr, int y, int x, byte defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static byte getOr(byte defaultValue, byte[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static short getOr(short[][] arr, int y, int x, short defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static short getOr(short defaultValue, short[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 	
 	/** Fetches an element or a default value from a 2D-array if the indices are out of bounds */
-	public static <T> T getOr(T[][] arr, int y, int x, T defaultValue) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
+	public static <T> T getOr(T defaultValue, T[][] arr, int y, int x) { return (y >= 0 && y < arr.length && x >= 0 && x < arr[y].length) ? arr[y][x] : defaultValue; }
 }
