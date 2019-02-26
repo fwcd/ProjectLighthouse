31b3b19b1d9494ecb54150e79d94cac0f4c2b591
fwcd <fwcdmail@gmail.com>
2019-02-27 00:45:26 +0100

    Consistent indentation in Game

--- src/main/java/lighthouse/model/Game.java
+++ src/main/java/lighthouse/model/Game.java
@@ -12,48 +12,48 @@ import lighthouse.util.ListenerList;
  * the current stage).
  */
 public class Game implements Ticker {
-    private static final Logger LOG = LoggerFactory.getLogger(Game.class);
-    private GameState state = new GameState();
+	private static final Logger LOG = LoggerFactory.getLogger(Game.class);
+	private GameState state = new GameState();
 	private GameStage stage = GameStages.CURRENT;
 	private Status status;
 	
 	private final ListenerList<GameStage> stageListeners = new ListenerList<>();
 	private final ListenerList<Status> statusListeners = new ListenerList<>();
 
-    private boolean won = false;
-    
-    public Game() {
-        setupListeners();
-    }
+	private boolean won = false;
+	
+	public Game() {
+		setupListeners();
+	}
 
-    @Override
-    public void tick() {
-        if (state.getLevel().isCompleted(state.getBoard())) {
-            LOG.info("Completed level");
-            won = true;
-        }
-    }
-    
-    private void setupListeners() {
-        state.getLevelListeners().add(level -> {
+	@Override
+	public void tick() {
+		if (state.getLevel().isCompleted(state.getBoard())) {
+			LOG.info("Completed level");
+			won = true;
+		}
+	}
+	
+	private void setupListeners() {
+		state.getLevelListeners().add(level -> {
 			// Update stage if the level has changed
 			stage.transitionFrom(stage, state);
 		});
-    }
+	}
 	
 	public GameState getState() { return state; }
 	
 	public GameStage getCurrentStage() { return stage; }
 	
 	public void switchToStage(GameStage newStage) {
-        if (stage != null && newStage.getIndex() != stage.getIndex()) {
+		if (stage != null && newStage.getIndex() != stage.getIndex()) {
 			newStage.transitionFrom(stage, state);
 		}
 		stage = newStage;
 		stageListeners.fire(newStage);
 	}
-    
-    public boolean isWon() { return won; }
+	
+	public boolean isWon() { return won; }
 	
 	public Status getStatus() { return status; }
 	
