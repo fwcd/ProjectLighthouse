c1c508a7eb028efb2f50b009216fe71b4603e237
fwcd <fwcdmail@gmail.com>
2019-02-27 15:11:44 +0100

    Removed redundant import

--- src/main/java/lighthouse/model/Board.java
+++ src/main/java/lighthouse/model/Board.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 import lighthouse.util.IntVec;
 import lighthouse.util.ListenerList;
