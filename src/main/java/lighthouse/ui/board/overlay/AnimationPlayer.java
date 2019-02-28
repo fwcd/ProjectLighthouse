611d0ac1a1bf971168f7459f4a277a797729fbdb
fwcd <fwcdmail@gmail.com>
2019-02-28 22:23:13 +0100

    Implemented AnimationPlayer

--- src/main/java/lighthouse/ui/board/overlay/AnimationPlayer.java
+++ src/main/java/lighthouse/ui/board/overlay/AnimationPlayer.java
@@ -1,25 +1,61 @@
 package lighthouse.ui.board.overlay;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.Timer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lighthouse.model.grid.WritableColorGrid;
 
-public class AnimationPlayer implements Overlay {
-	private List<AnimationState> states;
+/**
+ * A facility that plays animations on a high-
+ * resolution and a low-resolution output at
+ * a user-defined framerate.
+ */
+public class AnimationPlayer {
+	private static final Logger LOG = LoggerFactory.getLogger(AnimationPlayer.class);
+	private final List<AnimationState> states = new ArrayList<>();
+	private final Graphics2D g2d;
+	private final WritableColorGrid grid;
+	private Timer timer;
 	
-	@Override
-	public void drawHighRes(Graphics2D g2d) {
-		// TODO
+	public AnimationPlayer(Graphics2D g2d, WritableColorGrid grid) {
+		this.g2d = g2d;
+		this.grid = grid;
 	}
 	
-	@Override
-	public void drawLowRes(WritableColorGrid grid) {
-		// TODO
+	public void play(Animation animation) {
+		if (timer == null) {
+			LOG.warn("Called AnimationPlayer.play before starting the player.");
+		}
+		states.add(new AnimationState(animation));
 	}
 	
-	private static class AnimationState {
-		private int frame = 0;
-		private Animation animation;
+	private void renderFrame() {
+		Iterator<AnimationState> iterator = states.iterator();
+		while (iterator.hasNext()) {
+			AnimationState state = iterator.next();
+			if (state.hasFrame()) {
+				state.drawHighRes(g2d);
+				state.drawLowRes(grid);
+				state.advance();
+			} else {
+				// Remove animation that have finished playing
+				iterator.remove();
+			}
+		}
+	}
+	
+	public void start(int maxFPS) {
+		if (timer != null) {
+			timer.stop();
+		}
+		timer = new Timer(maxFPS, l -> renderFrame());
+		timer.start();
 	}
 }
