84b2f5dbff2d398fcc54c22e60fd3e6cfa1979bb
fwcd <fwcdmail@gmail.com>
2019-02-28 22:05:07 +0100

    Added Animation and AnimationPlayer

---
+++ src/main/java/lighthouse/ui/board/overlay/Animation.java
@@ -0,0 +1,13 @@
+package lighthouse.ui.board.overlay;
+
+import java.awt.Graphics2D;
+
+import lighthouse.model.grid.WritableColorGrid;
+
+public interface Animation {
+	int getTotalFrames();
+	
+	void drawHighRes(Graphics2D g2d, int frame);
+	
+	void drawLowRes(WritableColorGrid grid, int frame);
+}
---
+++ src/main/java/lighthouse/ui/board/overlay/AnimationPlayer.java
@@ -0,0 +1,17 @@
+package lighthouse.ui.board.overlay;
+
+import java.awt.Graphics2D;
+
+import lighthouse.model.grid.WritableColorGrid;
+
+public class AnimationPlayer implements Overlay {
+	@Override
+	public void drawHighRes(Graphics2D g2d) {
+		// TODO
+	}
+	
+	@Override
+	public void drawLowRes(WritableColorGrid grid) {
+		// TODO
+	}
+}
