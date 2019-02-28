aaf2585cca0bb2c58a3f0fdbcac695ca3ca12911
fwcd <fwcdmail@gmail.com>
2019-02-28 22:02:23 +0100

    Added overlay interface

---
+++ src/main/java/lighthouse/ui/board/overlay/Overlay.java
@@ -0,0 +1,15 @@
+package lighthouse.ui.board.overlay;
+
+import java.awt.Graphics2D;
+
+import lighthouse.model.grid.WritableColorGrid;
+
+/**
+ * An overlay that can be drawn in both high and low
+ * resolution.
+ */
+public interface Overlay {
+	void drawHighRes(Graphics2D g2d);
+	
+	void drawLowRes(WritableColorGrid grid);
+}
