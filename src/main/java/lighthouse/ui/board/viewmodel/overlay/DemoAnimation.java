6888939b8c114cfc459a67eaf26645f7cddf35b9
fwcd <fwcdmail@gmail.com>
2019-03-01 22:28:36 +0100

    Fancier demo animation

--- src/main/java/lighthouse/ui/board/viewmodel/overlay/DemoAnimation.java
+++ src/main/java/lighthouse/ui/board/viewmodel/overlay/DemoAnimation.java
@@ -1,20 +1,24 @@
 package lighthouse.ui.board.viewmodel.overlay;
 
 import java.awt.Color;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+
+import lighthouse.util.ColorUtils;
+import lighthouse.util.DoubleVec;
 
 public class DemoAnimation implements Animation {
 	@Override
 	public String getName() { return "Demo animation"; }
 	
 	@Override
-	public int getTotalFrames() { return 500; }
+	public int getTotalFrames() { return 300; }
 	
 	@Override
 	public List<OverlayShape> getShape(int frame) {
-		return Collections.singletonList(
-			new OverlayRect(1, frame / 40.0, 1, 1, Color.CYAN, OverlayShading.FILLED)
+		return Arrays.asList(
+			new OverlayRect(1, frame / 40.0, 1, 1, Color.CYAN, OverlayShading.FILLED),
+			new OverlayOval(new DoubleVec(2, 3), frame / 40.0, ColorUtils.withAlpha(((300 - frame) * 255) / 300, ColorUtils.LIGHT_VIOLET), OverlayShading.FILLED)
 		);
 	}
 }
