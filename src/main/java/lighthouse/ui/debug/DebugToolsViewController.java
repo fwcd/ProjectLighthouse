96408fa62852d3eea74827b4bfa32f86a72e5c69
fwcd <fwcdmail@gmail.com>
2019-03-01 22:21:36 +0100

    Fixed leaked listener in debug tools

--- src/main/java/lighthouse/ui/debug/DebugToolsViewController.java
+++ src/main/java/lighthouse/ui/debug/DebugToolsViewController.java
@@ -10,20 +10,27 @@ import lighthouse.ui.ViewController;
 import lighthouse.ui.debug.animations.ActiveAnimationsViewController;
 import lighthouse.ui.debug.listeners.ListenerGraphViewController;
 
+/**
+ * Manages useful tools for debugging.
+ */
 public class DebugToolsViewController implements ViewController, AutoCloseable {
 	private final WebTabbedPane component;
 	private final ListenerGraphViewController listenerGraph;
+	private final ActiveAnimationsViewController activeAnimations;
 	
 	public DebugToolsViewController(AppModel appModel, GameViewController gameVC) {
 		component = new WebTabbedPane();
 		
 		listenerGraph = new ListenerGraphViewController(appModel, gameVC);
 		component.addTab("Listeners", listenerGraph.getComponent());
-		component.addTab("Active Animations", new ActiveAnimationsViewController(gameVC.getBoard().getAnimationTracker()).getComponent());
+		
+		activeAnimations = new ActiveAnimationsViewController(gameVC.getBoard().getAnimationTracker());
+		component.addTab("Active Animations", activeAnimations.getComponent());
 	}
 	
 	@Override
 	public void close() {
+		activeAnimations.close();
 		listenerGraph.removeUpdateHooks();
 	}
 	
