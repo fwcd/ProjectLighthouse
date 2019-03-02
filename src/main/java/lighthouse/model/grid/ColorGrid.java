0baf52438ad5b5927a4136ca088f30e7fc3be571
fwcd <fwcdmail@gmail.com>
2019-03-02 01:54:16 +0100

    ColorGrid.getColorOr

--- src/main/java/lighthouse/model/grid/ColorGrid.java
+++ src/main/java/lighthouse/model/grid/ColorGrid.java
@@ -11,13 +11,17 @@ public interface ColorGrid {
 		return getColorAt(gridPos.getX(), gridPos.getY());
 	}
 	
-	default Color getColorOrBlackAt(IntVec gridPos) {
+	default Color getColorOr(Color defaultColor, IntVec gridPos) {
 		Color color = getColorAt(gridPos);
-		return (color == null) ? Color.BLACK : color;
+		return (color == null) ? defaultColor : color;
 	}
 	
-	default Color getColorOrBlackAt(int x, int y) {
+	default Color getColorOr(Color defaultColor, int x, int y) {
 		Color color = getColorAt(x, y);
-		return (color == null) ? Color.BLACK : color;
+		return (color == null) ? defaultColor : color;
 	}
+	
+	default Color getColorOrBlackAt(IntVec gridPos) { return getColorOr(Color.BLACK, gridPos); }
+	
+	default Color getColorOrBlackAt(int x, int y) { return getColorOr(Color.BLACK, x, y); }
 }
