fe9076985a52883e570c3a491c22f3a326b63a8c
fwcd <fwcdmail@gmail.com>
2019-03-02 01:08:05 +0100

    Generalized ArrayLists to List

--- src/main/java/lighthouse/alphabeta/ABMain.java
+++ src/main/java/lighthouse/alphabeta/ABMain.java
@@ -1,6 +1,7 @@
 package lighthouse.alphabeta;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import lighthouse.model.Board;
@@ -9,14 +10,15 @@ import lighthouse.model.Move;
 
 public abstract class ABMain {
 
-    public static ArrayList<Board> solve(Level toSolve) {
-        ArrayList<Board> moves = new ArrayList<>();
+    public static List<Board> solve(Level toSolve) {
+        List<Board> moves = new ArrayList<>();
         Board current = toSolve.getStart();
         Board goal = toSolve.getGoal();
-        ArrayList<Board> forbidden = new ArrayList<>();
+        List<Board> forbidden = new ArrayList<>();
         while (!current.equals(goal)) {
+            Board tmpCurrent = current;
             Optional<Move> nextMove = current.streamPossibleMoves()
-                    .filter(move -> !forbidden.contains(current.childBoard(move))).findFirst();
+                    .filter(move -> !forbidden.contains(tmpCurrent.childBoard(move))).findFirst();
             if (!nextMove.isPresent()){
                 moves.remove(moves.size()-1);
                 current = moves.get(moves.size()-1);
