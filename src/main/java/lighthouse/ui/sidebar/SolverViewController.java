1c2f7b50d7b8baa1a5a2fd9a04d884ef3efbc46a
fwcd <fwcdmail@gmail.com>
2019-03-02 19:15:49 +0100

    Customizable solver playback speed

--- src/main/java/lighthouse/ui/sidebar/SolverViewController.java
+++ src/main/java/lighthouse/ui/sidebar/SolverViewController.java
@@ -3,7 +3,11 @@ package lighthouse.ui.sidebar;
 import java.util.List;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpinnerNumberModel;
+
+import com.alee.laf.spinner.WebSpinner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,20 +30,27 @@ public class SolverViewController implements ViewController {
 	public SolverViewController(GameState gameState, BoardViewController board) {
 		this.gameState = gameState;
 		this.board = board;
+		
+		WebSpinner playbackSpeed = new WebSpinner(new SpinnerNumberModel(4, 1, 1000, 1));
+		
 		component = LayoutUtils.vboxOf(
+			LayoutUtils.compoundOf(
+				new JLabel("Playback speed (boards/sec): "),
+				playbackSpeed
+			),
 			LayoutUtils.panelOf(
-				LayoutUtils.buttonOf("Solve with Backtracking", this::backtrackingSolve)
+				LayoutUtils.buttonOf("Solve with Backtracking", () -> solveWithBacktracking((int) playbackSpeed.getValue()))
 			)
 		);
 	}
 	
-	private void backtrackingSolve() {
+	private void solveWithBacktracking(int boardsPerSecond) {
 		new Thread(() -> {
 			Solver solver = new BacktrackingSolver();
 			Level level = gameState.getLevel();
 			try {
 				List<Board> solution = solver.solve(level);
-				board.play(solution, 100);
+				board.play(solution, 1000 / boardsPerSecond);
 			} catch (Exception e) {
 				LOG.error("An error occurred while solving:", e);
 			}
