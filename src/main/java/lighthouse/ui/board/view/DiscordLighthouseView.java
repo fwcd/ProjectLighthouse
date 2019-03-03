f8fd7850b2766ed8182086b5df4e0aaa79c4a932
fwcd <fwcdmail@gmail.com>
2019-03-03 14:53:08 +0100

    Added Discord API dependency

---
+++ src/main/java/lighthouse/ui/board/view/DiscordLighthouseView.java
@@ -0,0 +1,10 @@
+package lighthouse.ui.board.view;
+
+import lighthouse.ui.board.viewmodel.LighthouseViewModel;
+
+public class DiscordLighthouseView implements LighthouseView {
+	@Override
+	public void draw(LighthouseViewModel viewModel) {
+		
+	}
+}
