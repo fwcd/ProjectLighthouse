bd6f1bfc4515f9258d50d15f044d1e9c49a922f0
fwcd <fwcdmail@gmail.com>
2019-03-03 03:05:44 +0100

    Added SineTable

---
+++ src/main/java/lighthouse/util/SinTable.java
@@ -0,0 +1,39 @@
+package lighthouse.util;
+
+/**
+ * A sine-/cosine value-table.
+ */
+public class SinTable {
+	private final int precision; // Values per degree
+	private final int mod;
+	private final float[] table;
+	
+	public SinTable() {
+		this(100);
+	}
+	
+	public SinTable(int precision) {
+		this.precision = precision;
+		
+		mod = 360 * precision;
+		table = new float[mod];
+		
+		for (int i=0; i<mod; i++) {
+			table[i] = (float) Math.sin((i * Math.PI) / (precision * 180));
+		}
+	}
+	
+	private float lookup(int i) {
+	    return i >= 0
+	    		? table[i % mod]
+	    		: -table[-i % mod];
+	}
+	
+	public float sinDeg(float angDeg) {
+		return lookup((int) (angDeg * precision + 0.5F));
+	}
+	
+	public float cosDeg(float angDeg) {
+		return lookup((int) ((angDeg + 90.0) * precision + 0.5F));
+	}
+}
