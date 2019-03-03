daa6879c70610adadf99f880d9a13e59c3d9d2f7
fwcd <fwcdmail@gmail.com>
2019-03-03 19:14:39 +0100

    Updated to java-discord-rpc 2.0.2 and implemented DiscordRPCRunner

---
+++ src/main/java/lighthouse/discordrpc/DiscordRPCRunner.java
@@ -0,0 +1,41 @@
+package lighthouse.discordrpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
+
+public class DiscordRPCRunner {
+	private static final Logger LOG = LoggerFactory.getLogger(DiscordRPCRunner.class);
+	private final DiscordRPC lib = DiscordRPC.INSTANCE;
+	
+	public void start() {
+		// Source: https://github.com/MinnDevelopment/java-discord-rpc#basics
+		String appID = "";
+		String steamID = "";
+		DiscordEventHandlers handlers = new DiscordEventHandlers();
+		handlers.ready = user -> LOG.info("Discord RPC is ready");
+		handlers.errored = (err, msg) -> LOG.info("Discord RPC errored with code {}: {}", err, msg);
+		handlers.disconnected = (err, msg) -> LOG.info("Discord RPC disconnected with code {}: {}", err, msg);
+		lib.Discord_Initialize(appID, handlers, true, steamID);
+		
+		DiscordRichPresence presence = new DiscordRichPresence();
+		presence.startTimestamp = System.currentTimeMillis() / 1000; // Epoch second
+		presence.details = "Lighthouse";
+		lib.Discord_UpdatePresence(presence);
+		
+		new Thread(this::runWorker, "Discord RPC").start();
+	}
+	
+	private void runWorker() {
+		while (!Thread.currentThread().isInterrupted()) {
+			LOG.trace("Running Discord RPC callbacks");
+			lib.Discord_RunCallbacks();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException ignored) {}
+		}
+	}
+}
